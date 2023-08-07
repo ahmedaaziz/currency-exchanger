223fb65cb5da55d1fe5a78d89c703a52e4be9886
Ahmed Abdelaziz <ahmed0abdelaziz@gmail.com>
2023-08-08 00:20:06 +0300

    adding font awesome module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 
 
 @NgModule({
   declarations: [
-    HeaderComponent
+    HeaderComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FontAwesomeModule
   ],
   exports:[
-    HeaderComponent
+    HeaderComponent,
+
   ]
 })
 export class SharedModule { }
