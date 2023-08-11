600ba7090420f106729b608799e6db343d893ba6
Ahmed Abdelaziz <ahmed0abdelaziz@gmail.com>
2023-08-11 18:30:55 +0300

    [appModule] injecting fontawesome,httpclient,sharedModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from "./shared/shared.module";
+import { CurrencyConvertPanelComponent } from './components/currency-convert-panel/currency-convert-panel.component';
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+import { DetailsComponent } from './components/details/details.component';
+import { HomeComponent } from './components/home/home.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CurrencyConvertPanelComponent,
+    DetailsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     SharedModule,
+    FontAwesomeModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
