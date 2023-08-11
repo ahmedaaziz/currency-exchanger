9b5c99ca7e159b2625501e139c7786236a0c9906
Ahmed Abdelaziz <ahmed0abdelaziz@gmail.com>
2023-08-11 18:31:44 +0300

    [Shared] Adding shared data files

---
+++ src/app/shared/topRatedCurrncies.ts
@@ -0,0 +1,39 @@
+export const popularCurrencies = [
+    {
+      name:'The Great British Pound',
+      symbol:'GBP'
+    },
+    {
+      name:'US Dollar',
+      symbol:'USD'
+
+    },
+    {
+      name:'EURO',
+      symbol:'EUR'
+    },
+    {
+      name:'The Japanese Yen',
+      symbol:'JPY'
+    },
+    {
+      name:'Australian Dollar',
+      symbol:'AUD'
+    },
+    {
+      name:'Canadian Dollar',
+      symbol:'CAD'
+    },
+    {
+      name:'Swiss Franc',
+      symbol:'CHF'
+    },
+    {
+      name:'Chinese Yuan',
+      symbol:'CNY'
+    },
+    {
+      name:'New Zealand Dollar',
+      symbol:'NZD'
+    },
+  ]
