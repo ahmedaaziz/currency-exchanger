8d9e2bf170224ca01f698a73bca240ee68b0618b
Ahmed Abdelaziz <ahmed0abdelaziz@gmail.com>
2023-08-12 20:56:14 +0300

    [header] add navigation to fixed routes

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { CurrenciesServiceService } from "src/app/services/currencies-service.service";
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   menu = faBars;
+  constructor(
+    private service:CurrenciesServiceService
+  ){}
 
+  ngOnInit(): void {
+
+  }
+
+  navigateTo(fromCurrency:string,toCurrency:string){
+    this.service.navigatToURL(fromCurrency,toCurrency)
+  }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,19 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { RouterModule } from "@angular/router";
+import { SpinnerComponent } from './components/spinner/spinner.component';
 
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
+    SpinnerComponent,
   ],
   imports: [
     CommonModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    RouterModule
   ],
   exports:[
     HeaderComponent,
+    SpinnerComponent,
 
   ]
 })
