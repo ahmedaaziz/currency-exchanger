fa32d59ea1c802974e6947cefae3d86c49c25874
Ahmed Abdelaziz <ahmed0abdelaziz@gmail.com>
2023-08-12 20:53:13 +0300

    [details] Add details logic

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
+import { from } from "rxjs";
+import { CurrenciesServiceService } from "src/app/services/currencies-service.service";
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.scss']
 })
-export class DetailsComponent {
+export class DetailsComponent implements OnInit {
+  currencyDetails:{} = {};
+  fromCurrency:string = '';
+  toCurrency:string = '';
+  // date:any = new Date().toJSON().slice(0,10);
+  today = new Date();
+  lastYear = new Date().getFullYear() - 1 ;
+  pastDate =new Date(this.lastYear,this.today.getMonth()+1,0).toJSON().slice(0,10)
+  historicalRate:any[] = [];
+  constructor(
+    private route:ActivatedRoute,
+    private service:CurrenciesServiceService
 
+  ){
+      this.route.params.subscribe(params => {
+      this.currencyDetails = params;
+      console.log(params);
+      this.fromCurrency = params['fromCurrency'];
+      this.toCurrency = params['toCurrency']
+    })
+  }
+  ngOnInit(): void {
+    console.log(this.pastDate);
+    this.historicalRates(this.pastDate,this.toCurrency);
+  }
+
+  historicalRates(date:string,toCurrency:string){
+    console.log('To Currency >> ', toCurrency);
+    this.service.getHistoricalRates(date,this.toCurrency).pipe().subscribe(
+      {
+      next:((data)=>{
+        let currencies = Object.keys(data)
+        for(var i = 0; i < currencies.length;i++) {
+        this.historicalRate.push({
+          currency: currencies[i],
+          rate: data[currencies[i]]
+        })
+        }
+        console.log(currencies);
+        console.log(this.historicalRate);
+
+      }),
+      error:((err)=>{
+        console.error(err.message)
+      }),
+      complete:(()=>{})
+    })
+  }
 }
